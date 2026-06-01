78261b87164010e127da41f979b9878c533ea69b
Stefan6666s <sapostolov08@gmail.com>
2026-06-01 11:00:47 +0300

    added utils logic

---
+++ server/utils/createLog.js
@@ -0,0 +1,9 @@
+// Create Log Utility
+// Handles creation of activity logs
+
+const createLog = async (data) => {
+  // Add log creation logic here
+  // Example: new Log(data).save()
+};
+
+module.exports = createLog;
---
+++ server/utils/generateToken.js
@@ -0,0 +1,10 @@
+// Generate Token Utility
+// Handles JWT token generation
+
+const jwt = require('jsonwebtoken');
+
+const generateToken = (payload, expiresIn = '7d') => {
+  return jwt.sign(payload, process.env.JWT_SECRET, { expiresIn });
+};
+
+module.exports = generateToken;
